ba06d67ae4b1473b4ebc38d54f8ec36dd1d455ab
klelia <clelia.fradella@gmail.com>
2024-11-13 14:06:29 +0100

    draw card

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
-const teamMembers = [
-  {
-    name: "Wayne Barnett",
-    role: "Founder & CEO",
-    image: "wayne-barnett-founder-ceo.jpg",
-  },
-  {
-    name: "Angela Caroll",
-    role: "Chief Editor",
-    image: "angela-caroll-chief-editor.jpg",
-  },
-  {
-    name: "Walter Gordon",
-    role: "Office Manager",
-    image: "walter-gordon-office-manager.jpg",
-  },
-  {
-    name: "Angela Lopez",
-    role: "Social Media Manager",
-    image: "angela-lopez-social-media-manager.jpg",
-  },
-  {
-    name: "Scott Estrada",
-    role: "Developer",
-    image: "scott-estrada-developer.jpg",
-  },
-  {
-    name: "Barbara Ramos",
-    role: "Graphic Designer",
-    image: "barbara-ramos-graphic-designer.jpg",
-  },
-];
+import { getRndInteger } from "./utilities.js";
+import { teamMembers } from "./db.js";
+
+const myform = document.getElementById("addForm");
+myform.addEventListener("submit", addMember);
+drawCard();
+
+function addMember(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+  const image = document.getElementById("image").value;
+
+  const newMember = {
+    name,
+    role,
+    image,
+  };
+  console.log(newMember);
+  teamMembers.push(newMember);
+  drawCard();
+}
+
+function drawCard() {
+  let template = "";
+  const teamContainer = document.querySelector(".team-container");
+
+  for (let value of teamMembers) {
+    template += `
+    <div class="ccol-12 col-md-6 col-lg-4">
+    <div class="team-card">
+        <div class="card-image">
+              <img src="img/${value.image}" alt="${value.name}" />
+          </div>
+          <div class="card-text">
+              <h3>${value.name}</h3>
+              <p>${value.role}</p>
+          </div>
+      </div>
+    </div>
+  `;
+  }
+
+  teamContainer.innerHTML = template;
+}
